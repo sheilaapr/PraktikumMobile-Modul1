b3d13cd69ae58811d3d19e95aa526aa376860ccb
sheilaapr <230605110005@student.uin-malang.ac.id>
2025-09-09 22:51:09 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Program Pertamaku"),
+        ),
+        body: const Center(
+          child: Text(
+            // "Hello World",
+            "Sheila Apriliani Putri",
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
